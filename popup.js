5c400d8ff42c1a5453757c59fe2ac2ff72bf7dea
Kyle <kyle.c.r.fahey@gmail.com>
2017-08-05 18:30:14 -0500

    nonono

--- popup.js
+++ popup.js
@@ -29,7 +29,7 @@ fetch('http://localhost:3000')
   .catch(function(err) {  
     console.log('Fetch Error :-S', err);  
   });
- 
+  
 
 // Copyright (c) 2014 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
